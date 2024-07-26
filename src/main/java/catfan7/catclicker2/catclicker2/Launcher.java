8b936e783bee9a5121737317edac6a386c0e8d46
Catfan7 <catclips.sussymogus@gmail.com>
2024-07-26 12:44:53 -0700

    Added launcher file

---
+++ src/main/java/catfan7/catclicker2/catclicker2/Launcher.java
@@ -0,0 +1,9 @@
+package catfan7.catclicker2.catclicker2;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args){
+        Application.launch(Main.class, args);
+    }
+}
